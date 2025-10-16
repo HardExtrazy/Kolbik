c0c372c8b58ba5113da6df790605b482576b7d91
HardExtrazy <Kolbik12@gmail.com>
2025-10-16 19:45:25 +0300

    Лабораторная работа №2

---
+++ Kolbik.Domain/Entities/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolbik.Domain.Entities
+{
+    public class Author
+    {
+        //Id Автора
+        public uint Id { get; set; }
+        //Имя автоа (В случае отсутствия будет пустая строка)
+        public string FirstName { get; set; } = string.Empty;
+        //Фамилия автоа (В случае отсутствия будет пустая строка)
+        public string LastName { get; set; } = string.Empty;
+        //Отчество автора
+        public string? SureName { get; set; }
+        //Возраст
+        public byte Age { get; set; }
+        //Путь к фотографии
+        public string? PathPhoto {  get; set; }
+        //Коллекция книг (В случае отсутствия будет пустой список)
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+
+    }
+}
---
+++ Kolbik.Domain/Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolbik.Domain.Entities
+{
+    public class Book
+    {
+        //Id книги
+        public uint Id { get; set; }
+        //Название книги (В случае отсутствия будет пустая строка)
+        public string Name { get; set; } = string.Empty;
+        //Краткое описание книги (В случае отсутствия будет пустая строка)
+        public string Description { get; set; } = string.Empty;
+        //Рейтинг книги
+        public short Rating { get; set; }
+        //Id Автора (Вторичный ключ)
+        public uint AuthorId { get; set; }
+        //Автор книги (В случае отсутствия будет новый автор)
+        public Author? Author { get; set; } = new Author();
+    }
+}
