b2227b5f1bac4087bf128659be452e68a781d096
HardExtrazy <Kolbik12@gmail.com>
2025-12-20 20:56:49 +0300

    лабораторная 6

---
+++ Kolbik.Blazor/Services/ApiBookService.cs
@@ -0,0 +1,45 @@
+﻿using Kolbik.Domain.Entities;
+using Kolbik.Domain.Models;
+using static System.Net.WebRequestMethods;
+
+namespace Kolbik.Blazor.Services
+{
+    public class ApiBookService(HttpClient Http): IBookService<Book>
+    {
+        List<Book> _books = new();
+        int _currentPage = 1;
+        int _totalPages = 1;
+        public IEnumerable<Book> Books => _books;
+        public int CurrentPage => _currentPage;
+        public int TotalPages => _totalPages;
+        public event Action ListChanged;
+        public async Task GetBooks(int pageNo = 1)
+        {
+            // Отправить запрос http
+            var result = await Http.GetAsync(Http.BaseAddress.AbsoluteUri);
+            // В случае успешного ответа
+            if (result.IsSuccessStatusCode)
+            {
+                // получить данные из ответа
+                var responseData = await result.Content
+                .ReadFromJsonAsync<ResponseData<List<Book>>>();
+                // обновить параметры страниц
+                _currentPage = pageNo;
+                _totalPages = (int)Math.Ceiling(responseData.Data.Count() / (double)3);
+                // получить нужную страницу
+                _books = responseData.Data
+                .Skip((pageNo - 1) * 3)
+                .Take(3)
+                .ToList();
+                ListChanged?.Invoke();
+            }
+            // В случае ошибки
+            else
+            {
+                _books = null;
+                _currentPage = 1;
+                _totalPages = 0;
+            }
+        }
+    }
+}
---
+++ Kolbik.Blazor/Services/IBookService.cs
@@ -0,0 +1,15 @@
+﻿namespace Kolbik.Blazor.Services
+{
+    public interface IBookService<T> where T : class
+    {
+        event Action ListChanged;
+        // Список объектов
+        IEnumerable<T> Books { get; }
+        // Номер текущей страницы
+        int CurrentPage { get; }
+        // Общее количество страниц
+        int TotalPages { get; }
+        // Получение списка объектов
+        Task GetBooks(int pageNo = 1);
+    }
+}
